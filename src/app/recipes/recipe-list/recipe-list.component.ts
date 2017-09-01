e6e53d8e19df6bee9c4b15354b4b348fe1eb3d2d
Patrick Ritskes <patrickr@softpak.local>
2017-09-01 10:20:47 +0200

    Add Recipe Service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
+import { RecipeService } from '../recipe.service';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -9,17 +11,12 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-
-  recipes: Recipe[] = [
-    new Recipe('Spaghetti', 'Spaghetti Napolitana', 'https://upload.wikimedia.org/wikipedia/commons/2/2a/Spaghetti_al_Pomodoro.JPG'),
-    new Recipe('Tortellini', 'Tortellini with Spinach', 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Tortellini.jpg'),
-    new Recipe('Olive Bread', 'Olive Bread with olive oil', 'https://c2.staticflickr.com/4/3184/2302819771_8c6006e638_z.jpg?zz=1'),
-    new Recipe('Tomato Soup', 'Tomato cream soup', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Potage_Cr%C3%A9cy_soup.jpg')
-  ];
-
-  constructor() { }
+  recipes: Recipe[];
+  
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe) {
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,14 @@
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+    private recipes: Recipe[] = [
+        new Recipe('Spaghetti', 'Spaghetti Napolitana', 'https://upload.wikimedia.org/wikipedia/commons/2/2a/Spaghetti_al_Pomodoro.JPG'),
+        new Recipe('Tortellini', 'Tortellini with Spinach', 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Tortellini.jpg'),
+        new Recipe('Olive Bread', 'Olive Bread with olive oil', 'https://c2.staticflickr.com/4/3184/2302819771_8c6006e638_z.jpg?zz=1'),
+        new Recipe('Tomato Soup', 'Tomato cream soup', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Potage_Cr%C3%A9cy_soup.jpg')
+    ];
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
+}
